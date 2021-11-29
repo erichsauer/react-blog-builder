b4d06f1e4b7024098fed0fc7cfc23ea501f6c12f
Erich Sauer <erich.sauer@gmail.com>
2021-11-29 14:50:01 -0800

    fin

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Preview from '../../components/Preview/Preview'
 import Editor from '../../components/Editor/Editor'
@@ -7,12 +7,30 @@ import './Home.css'
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('')
+  const [subtitle, setSubtitle] = useState('')
+  const [font, setFont] = useState('')
+  const [align, setAlign] = useState('')
+  const [text, setText] = useState('')
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview {...{ title, subtitle, font, align, text }} />
+      <Editor
+        {...{
+          title,
+          subtitle,
+          font,
+          align,
+          text,
+          setTitle,
+          setSubtitle,
+          setFont,
+          setAlign,
+          setText,
+        }}
+      />
     </main>
   )
 }
